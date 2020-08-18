6b3a078d0f25518f0c4e943fe16932cc587d27f6
AdrewJons <69836527+AdrewJons@users.noreply.github.com>
2020-08-18 11:39:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hello,
-This is a Telegram File convert Bot!
+This is a Telegram File convert Bot Created And Edited by [THIS PERSON](https://tx.me/Adrewjons)
 You can convert any Telegram Document, using this bot! With custom thumbnail
      
     """
@@ -10,29 +10,29 @@ You can convert any Telegram Document, using this bot! With custom thumbnail
     DOWNLOAD_START = "trying to download"
     UPLOAD_START = "trying to upload"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/Orderofmovies"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_CUSTOM_THUMB_NAIL = "Your custom Thumbnail saved 😎.This image will be used in next 24 hr"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom Thumbnail cleared ☠️."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = ""
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found noob."
     CURENT_PLAN_DETAILS = """Current plan details
 --------
 Telegram ID: <code>{}</code>
-Plan name: <a href='https://t.me/InFoTelGroup/174'>{}</a>
+Plan name: <a href='https://t.me/OrderOfMovies/12318'>{}</a>
 Expires on: {}"""
     HELP_USER = """I can do:
-👉 <a href="https://telegram.dog/FileConvertRobot">Convert To Streamable video any telegram file</a>
+👉 <a href="https://telegram.dog/Convert2_Robot">Convert To Streamable video any telegram file</a>
 --------
 
 🔥 How to deploy : <a href='https://youtu.be/zQamSjXBpJU'>Click here</a>
 
-    © Group   : @InfotelGroup
-    © Channel : @DX_BotZ 
-    © credits : @Spechide
+    © Group   : @Orderofmovies
+    © Channel : @XstreamMovies 
+    © credits : @AdrewJ
 
 Send /me to know current plan details
 
